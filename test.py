b97544cd0441846b38c25589c82e06c1f33ad5ab
leoharel <leoharel42@gmail.com>
2025-10-02 09:55:29 +0200

    premier test + stat unites

--- test.py
+++ test.py
@@ -1,2 +1,28 @@
-#Bibliothèques
-import pygame
+import csv
+with open('Stats_Units.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter='\t')
+    for row in spamreader:
+        print(', '.join(row))
+
+
+
+
+
+
+
+
+class Unit():
+    def __init__(self):
+        All_Stats
+        Team
+
+    def deplacement():
+        pass
+
+    def degats_infliges():
+        pass
+
+    def degats_pris():
+        pass
+
+    
