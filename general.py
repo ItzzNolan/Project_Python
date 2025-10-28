d6f942ea00af1b80caa260e1ca01a81055ec3245
ItzzNolan <nolan.02g@gmail.com>
2025-10-28 10:06:53 +0100

    Import Libraries + Docs & Introduction to General Classes

---
+++ general.py
@@ -0,0 +1,20 @@
+############################################
+############ Made By Nolan Grlt ############
+############################################
+
+"""On importe des utilitaires standards : dataclass pour des conteneurs d'Actions, Enum pour les types d'action, 
+Protocol pour définir des interfaces minimales que le moteur (IA) devra satisfaire"""
+from dataclasses import dataclass
+from enum import auto, Enum
+from typing import Iterable, List, Optional, Tuple, Dict, Any, Protocol
+
+import abc
+
+#Type alias pour les coordonées de la grille (x,y)
+Cord = Tuple[int, int]
+
+class General(abc.ABC):
+    """Classe de base pour les généraux tactiques.
+    Le moteur doit appeler decider_actions a chaque tick en passant
+    un itérable d'unités alliés et une GameView"""
+    pass
