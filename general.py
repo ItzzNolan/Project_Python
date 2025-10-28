b9b9512915593d31006ae657e4cd8cea837333ae
ItzzNolan <nolan.02g@gmail.com>
2025-10-28 10:37:09 +0100

    Creation Interfaces IA - General's Adaptations

--- general.py
+++ general.py
@@ -10,8 +10,48 @@ from typing import Iterable, List, Optional, Tuple, Dict, Any, Protocol
 
 import abc
 
-#Alias simple pour représenter une position sur la grille (x,y)
-Cord = Tuple[int, int]
+#Alias Tuple pour représenter une position sur la grille (x,y)
+Cord = Tuple[int,int]
+
+"""Interface que le moteur doit implementer ou adapter lorsqu'il appelle l'<<IA>>"""
+class UnitView(Protocol):
+    id:int
+    #Identifiant du joueur -->
+    owner:int
+    #Coordonnees de l’unit -->
+    @property
+    def pos(self) -> Cord:...
+    #Unit toujours vivante -->
+    @property
+    def is_alive(self) -> bool:...
+
+"""Tests UnitView -->
+
+class Cord:
+    def __init__(self, x:int, y:int):
+        self.x = x
+        self.y = y
+
+class Soldier:
+    def __init__(self, id:int, owner:int, pos:Cord):
+        self.id = id
+        self.owner = owner
+        self._pos = pos
+        self._hp = 100
+
+    @property
+    def pos(self) -> Cord:
+        return self._pos
+
+    @property
+    def is_alive(self) -> bool:
+        return self._hp > 0
+"""
+"""Interface de la vue du jeu"""
+class GameView(Protocol):
+    #Compteur de ticks (ou de tours) dans le jeu
+    @property
+    def tick(self) -> int:...
 
 class General(abc.ABC):
     """Classe de base pour les généraux tactiques.
