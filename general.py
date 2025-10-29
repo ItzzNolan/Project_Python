d603e97f7139dff40218b6a8ace4091d9ab66907
ItzzNolan <nolan.02g@gmail.com>
2025-10-29 10:33:04 +0100

    Adds Methods Unit & Game Views

--- general.py
+++ general.py
@@ -13,17 +13,36 @@ import abc
 #Alias Tuple pour représenter une position sur la grille (x,y)
 Cord = Tuple[int,int]
 
+# ----------------------------
+#         Protocoles
+# ----------------------------
+
 """Interface que le moteur doit implementer ou adapter lorsqu'il appelle l'<<IA>>"""
 class UnitView(Protocol):
     id:int
+
     #Identifiant du joueur -->
     owner:int
-    #Coordonnees de l’unit -->
+
+    #Coordonnees de l'unit -->
     @property
     def pos(self) -> Cord:...
+
     #Unit toujours vivante -->
     @property
     def is_alive(self) -> bool:...
+
+    #HP de l'unit -->
+    @property
+    def hp(self) -> int:...
+
+    #Attaque dans la portee (tiles) -->
+    @property
+    def range(self) -> int:...
+
+    def can_attack(self) -> bool:... #cooldown ready
+
+
 
 
 """Tests UnitView -->
@@ -56,6 +75,26 @@ class GameView(Protocol):
     @property
     def tick(self) -> int:...
 
+    #Renvoie la liste des ennemis dans la ligne de vue d'une unite
+    def enemy_in_los(self, unit:UnitView) -> List[UnitView]:...
+
+    #Renvoie l'ennemi le plus proche d’une unite, ou None s'il n'y en a pas
+    def nearest_enemy(self, unit:UnitView) -> Optional[UnitView]:...
+
+    #Renvoie toutes les unites ennemies visibles par un joueur donne
+    def all_seen_enemies(self, id_player:int) -> List[UnitView]:...
+
+    #Calcule la distance (en nombre de cases) entre deux coord
+    def distance_tiles(self, x:Cord, b:Cord) -> int:...
+
+    #Indique si une case donnee peut etre traversee ou non
+    def is_walkable(self, a:Cord) -> bool:...
+
+
+# ----------------------------
+#           Actions
+# ----------------------------
+
 class TypeAction(Enum):
     """Type d'actions que le general peut retourner"""
     HOLD = auto()
@@ -78,6 +117,10 @@ class Action:
             object.__setattr__(self,"dic",{})
     """
 
+# ----------------------------
+#    Classe de base General
+# ----------------------------
+
 class General(abc.ABC):
     """Classe de base pour les généraux tactiques.
     Le moteur doit appeler decider_actions a chaque tick en passant
@@ -96,6 +139,10 @@ class General(abc.ABC):
         raise NotImplementedError
     
 
+# ----------------------------
+#       Generaux du jeu
+# ----------------------------
+
 class CaptainBraindead(General):
     """General qui ne donne aucun ordre et qui sert de baseline"""
 
@@ -107,3 +154,4 @@ class CaptainBraindead(General):
         Le moteur doit interpreter ça comme << laisser les unites suivre leur comportement par defaut >>
         """
         return []
+    
