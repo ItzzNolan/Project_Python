0d1dffb90ea6f01b26d323d03ae7967f13c9768e
hatam3009-cmyk <hatam3009@gmail.com>
2025-11-24 14:58:44 +0100

    Update MajorDaft.py

--- MajorDaft.py
+++ MajorDaft.py
@@ -1,50 +1,38 @@
 #Ci-dessous se trouve la classe Major DAFT.
 #Nolan a créé une version de ce commandant plus intelligente, donc j’ai modifié un peu le code.
 #Vous pouvez proposer un nouveau nom pour le commandant de Nolan.
+class MajorDAFT(General):
+    """
+    Major DAFT: Dumb-As-Fuck-Tactician
+    Chaque unité attaque l'ennemi le plus proche ou se déplace vers lui sans aucune autre considération.
+    """
 
-class MajorDaft:
-    def __init__(self, portee):
-        self.portee = portee
+    def __init__(self, id_player: int):
+        super().__init__(name="Major DAFT", id_player=id_player)
 
-    def distance(self, u1, u2):
-        return abs(u1.x - u2.x) + abs(u1.y - u2.y)   # distance 
+    def decider_actions(self, unit_ally: Iterable[UnitView], game: GameView) -> List[Action]:
+        actions: List[Action] = []
 
-    def direction_vers(self, u1, u2):
-        """Retourne un vecteur (dx, dy) indiquant la direction vers l’ennemi."""
-        dx = 0
-        dy = 0
-        if u2.x > u1.x:
-            dx = 1
-        elif u2.x < u1.x:
-            dx = -1
-
-        if u2.y > u1.y:
-            dy = 1
-        elif u2.y < u1.y:
-            dy = -1
-
-        return (dx, dy)
-
-    def decider_actions(self, unites_alliees, unites_ennemies):
-        actions = []
-
-        for u_allie in unites_alliees:
+        for unit in unit_ally:
+            if not unit.is_alive:
+                continue
 
             # Trouver l'ennemi le plus proche
-            ennemi_proche = min(
-                unites_ennemies,
-                key=lambda u_ennemi: self.distance(u_allie, u_ennemi)
-            )
-            dist = self.distance(u_allie, ennemi_proche)
+            target = self._closest_enemies(unit, game)
 
-            #  Si l’ennemi est à portée → attaquer
-            if dist <= self.portee:
-                actions.append(('ATTAQUER', ennemi_proche))
+            if target is None:
+                # Aucun ennemi visible → unit reste sur place
+                actions.append(self._order_hold(unit))
+                continue
 
-            # Sinon → se déplacer vers l'ennemi
+            # Calcul distance
+            dist = game.distance_tiles(unit.pos, target.pos)
+
+            if dist <= unit.range:
+                # Si l'ennemi est à portée → attaque
+                actions.append(self._order_attack_focus(unit, target))
             else:
-                direction = self.direction_vers(u_allie, ennemi_proche)
-                actions.append(('DEPLACER', direction))
+                # Sinon → se déplacer vers l'ennemi
+                actions.append(self._order_move_to(unit, target.pos))
 
         return actions
-
